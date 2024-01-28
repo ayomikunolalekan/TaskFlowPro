273e86876079f7d811d59df723551c4880d0679a
Raymond Adu <raymond.aduadjei@kibo.school>
2024-01-28 18:17:20 +0000

    create database commit

--- app.py
+++ app.py
@@ -1,7 +1,43 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///taskflopro.db"
+db = SQLAlchemy(app)
 
+with app.app_context():
+    
+    class User(db.Model):
+        first_name =  db.Column(db.String(50), nullable=False)
+        last_name = db.Column(db.String(50), nullable=False)
+        email = db.Column(db.String(128), unique=True, nullable=False, primary_key=True)
+        password = db.Column(db.String(64), nullable=False)
+        tasks = db.relationship("Task", backref = "user",  lazy=True)
+
+        def __init__  (self, first_name, last_name, email, password):
+            self.first_name = first_name
+            self.last_name = last_name
+            self.email = email
+            self.password = password
+
+    class Task(db.Model):
+        id = db.Column(db.Integer, primary_key= True)
+        title = db.Column(db.String(100), nullable= False)
+        start = db.Column(db.DateTime, nullable= False, default=datetime.datetime.now())
+        end = db.Column(db.DateTime, nullable= False)
+        status = db.Column(db.String(100), nullable= False)
+        user_id = db.Column(db.Integer, db.ForeignKey("user.email"), nullable= False)
+
+        def __init__(self, title,start,end,status,user_id):
+            self.title = title
+            self.start = start
+            self.end = end
+            self.status = status
+            self.user_id = user_id
+
+    db.create_all()
+        
 @app.route("/")
 def homepage():
     return render_template("home.html")
@@ -17,3 +53,8 @@ def signup_page():
 @app.route("/dashboard")
 def dashboard_page():
     return render_template("dashboard.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+    
