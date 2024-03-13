f8d96d20d5dc7594042bbc7e3b01d9f64266804e
Victor Idowu <victoroluwapelumi504@gmail.com>
2024-03-13 13:52:08 +0100

    Updated version 1

---
+++ website/static/js/dashboard.js
@@ -0,0 +1,15 @@
+// // script.js
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   // Wait for the DOM content to be fully loaded
+
+//   // Create a new i element with Font Awesome classes
+//   var barsIcon = document.createElement('i');
+//   barsIcon.className = 'fas fa-bars';
+
+//   // Get the label element by its 'for' attribute
+//   var labelElement = document.querySelector('label[for="check"]');
+
+//   // Append the Font Awesome icon inside the label
+//   labelElement.appendChild(barsIcon);
+// });
